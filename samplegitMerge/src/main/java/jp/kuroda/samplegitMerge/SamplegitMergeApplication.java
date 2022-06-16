1d100dbf659f6859bc4491ec27b67ff68b482217
kurodachikara <85984296+kurodachikara@users.noreply.github.com>
2022-06-16 11:37:20 +0900

    first commit

---
+++ samplegitMerge/src/main/java/jp/kuroda/samplegitMerge/SamplegitMergeApplication.java
@@ -0,0 +1,13 @@
+package jp.kuroda.samplegitMerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplegitMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplegitMergeApplication.class, args);
+	}
+
+}
---
+++ samplegitMerge/src/test/java/jp/kuroda/samplegitMerge/SamplegitMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kuroda.samplegitMerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplegitMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
